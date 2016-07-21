25f19738bd79c652312553e57414af3862f444fe
VictorEunt <VictorEstrada@my.unt.edu>
2016-07-21 17:31:10 -0500

    masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.turbolinks
+//= require turbolinks
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
-//= require turbolinks
 //= require_tree .
